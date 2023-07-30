50f72f1543e8e0faad5f35d826301a8f1f75a200
Dhanush H V <dhanushhv75@gmail.com>
2023-07-30 14:09:07 +0530

    Added new methods for arrays and strings

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,28 @@ class Main {
         return rev;
     }
 
-    public static void main(String[] args) {
-        String str = "hello";
-        String rev = reverseString(str);
+    protected static int[] reverseArray(int[] arr) {
+        final int size = arr.length;
+        int i, temp;
 
-        System.out.println(rev);
+        for (i = 0; i < size / 2; i++) {
+            temp = arr[i];
+            arr[i] = arr[size-i-1];
+            arr[size-i-1] = temp;
+        }
+
+        return arr;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int e : arr)
+            System.out.print(e + " ");
+    }
+
+    public static void main(String[] args) {
+        final int[] arr = {1, 2, 3, 4, 5};
+        int[] rev = reverseArray(arr);
+
+        printArray(rev);
     }
 }
