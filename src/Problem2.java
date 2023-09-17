00a39b6e7e8823b11849c7319a642306dbc5ec9b
DHANUSH H V <dhanushhv75@gmail.com>
2023-09-17 17:10:13 +0530

    ADDED PROBLEM2.JAVA

--- src/Problem2.java
+++ src/Problem2.java
@@ -17,19 +17,27 @@ public class Problem2 {
         for (int i = 0; i < 5; i++)
             stack2.push(sc.nextInt());
 
-        // New Stack to store both the stacks
-        Stack<Integer> newStack = new Stack<>(stack1.size() + stack2.size());
-
-        // Add the elements of Stack1 to newStack, ignore if it is a duplicate
-        for (int i = 0; i < stack1.top + 1; i++)
-            newStack.push(stack1.getValue(i));
-
-        // Add the element of Stack2 to newStack, ignore if it is a duplicate
-        for (int i = 0; i < stack2.top + 1; i++)
-            newStack.push(stack2.getValue(i));
+        Stack<Integer> newStack = filter(stack1, stack2);
 
         // Print the new Stack
         newStack.printStack();
         sc.close();
+    }
+
+    public static Stack<Integer> filter(Stack<Integer> stack1, Stack<Integer> stack2) {
+        HashSet<Integer> stacks = new HashSet<>();
+
+        for (int i = 0; i < stack1.top + 1; i++)
+            stacks.add(stack1.getValue(i));
+
+        for (int i = 0; i < stack2.top + 1; i++)
+            stacks.add(stack2.getValue(i));
+
+        Stack<Integer> newStack = new Stack<>(stacks.size());
+
+        for (Integer stack : stacks)
+            newStack.push(stack);
+
+        return newStack;
     }
 }
