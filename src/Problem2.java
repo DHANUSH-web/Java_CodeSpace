aa2413c13168933d6d196084c1440338e703501c
DHANUSH H V <dhanushhv75@gmail.com>
2023-09-17 16:37:41 +0530

    UPDATED

--- src/Problem2.java
+++ src/Problem2.java
@@ -1,13 +1,9 @@
-// Find the common in two stacks
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Scanner;
+import java.util.HashSet;
 
-public class Problem1 {
-
+public class Problem2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         Stack<Integer> stack1 = new Stack<>(5);
         Stack<Integer> stack2 = new Stack<>(5);
 
@@ -21,27 +17,19 @@ public class Problem1 {
         for (int i = 0; i < 5; i++)
             stack2.push(sc.nextInt());
 
-        // Find the common between two stacks and store in a new stack
-        Stack<Integer> commons = findCommon(stack1, stack2);
-        commons.printStack();
-        sc.close();
-    }
-
-    public static Stack<Integer> findCommon(Stack<Integer> stack1, Stack<Integer> stack2) {
-        HashSet<Integer> commons = new HashSet<>();
+        // New Stack to store both the stacks
         Stack<Integer> newStack = new Stack<>(stack1.size() + stack2.size());
 
+        // Add the elements of Stack1 to newStack, ignore if it is a duplicate
         for (int i = 0; i < stack1.top + 1; i++)
-            commons.add(stack1.getValue(i));
+            newStack.push(stack1.getValue(i));
 
+        // Add the element of Stack2 to newStack, ignore if it is a duplicate
         for (int i = 0; i < stack2.top + 1; i++)
-            commons.add(stack2.getValue(i));
+            newStack.push(stack2.getValue(i));
 
-        Iterator<Integer> it = commons.iterator();
-
-        while (it.hasNext())
-            newStack.push(it.next());
-
-        return newStack;
+        // Print the new Stack
+        newStack.printStack();
+        sc.close();
     }
 }
