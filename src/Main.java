bb5b6d9c4141ad25d1c5e77eeb4878bd33ec15bc
Dhanush H V <dhanushhv75@gmail.com>
2023-10-23 01:51:25 +0530

    Update: Implemented HashTable

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,27 @@
-class Main {
+public class Main {
     public static void main(String[] args) {
-        DoublyLinkedList<Integer> nodes = new DoublyLinkedList<>();
+        // Create a hash table with a size of 10
+        HashTable hashTable = new HashTable(10);
 
-        nodes.addLast(100);
-        nodes.addLast(80);
-        nodes.addLast(40);
-        nodes.addLast(10);
+        // Insert key-value pairs into the hash table
+        hashTable.insert("Alice", "123-456-7890");
+        hashTable.insert("Bob", "234-567-8901");
+        hashTable.insert("Charlie", "345-678-9012");
 
-        Iterator<Integer> iter = nodes.iterator();
+        // Retrieve values using keys
+        String aliceNumber = hashTable.search("Alice");
+        String bobNumber = hashTable.search("Bob");
+        String davidNumber = hashTable.search("David"); // Returns null for non-existing key
 
-        do {
-            System.out.println(iter.next() / 2);
-        } while (iter.hasNext());
+        // Output retrieved values
+        System.out.println("Alice's number: " + aliceNumber); // Output: 123-456-7890
+        System.out.println("Bob's number: " + bobNumber); // Output: 234-567-8901
+        System.out.println("David's number: " + davidNumber); // Output: null
+
+        // print the values using Iterator
+        Iterator<String> it = hashTable.iterator();
+
+        while (it.hasNext())
+            System.out.println(it.next());
     }
 }
